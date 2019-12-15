0c72ff549f131e12652e75a0f3d735fb97a77d62
Elias <MagicElyas@users.noreply.github.com>
2019-12-15 12:01:36 +0100

    Basic MLP created - It now is complete to be used to train and test the MNIST dataset.

--- neural/mlp.py
+++ neural/mlp.py
@@ -1,6 +1,7 @@
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.datasets import mnist
+from keras.utils import to_categorical
 import keras
 from sklearn.preprocessing import LabelBinarizer
 import matplotlib.pyplot as plt
@@ -18,16 +19,19 @@ def generate_mlp(dim, num_outputs):
   return model
 
 def load_data():
-  (x_train,x_test), (y_train,y_test) = mnist.load_data()
-  return x_train, x_test, y_train, y_test 
+  (x_train,y_train), (x_test,y_test) = mnist.load_data()
+  return x_train, y_train, x_test, y_test 
 
 
 if __name__ == '__main__':
-  x_train, x_test, y_train, y_test = load_data()
+  x_train, y_train, x_test, y_test = load_data()
   x_train = x_train.reshape(x_train.shape[0], x_train.shape[1]*x_train.shape[2])
-  y_train = y_train.reshape(y_train.shape[0], y_train.shape[1]*y_train.shape[2])
+  x_test = x_test.reshape(x_test.shape[0], x_test.shape[1]*x_test.shape[2])
+  y_train = to_categorical(y_train)
+  y_test = to_categorical(y_test)
   model = generate_mlp(28*28,10)
   model.compile(loss= 'categorical_crossentropy', optimizer='adam', metrics=['acc'])
   model.summary()
-  #One hot required
-  # model.fit(x=x_train, y=y_train, verbose=1)
+  model.fit(x=x_train, y=y_train, verbose=1, epochs=15)
+  h = model.evaluate(x=x_test,y=y_test)
+  
