25c7e9f570fb315117a16b54444e128cc72e23ca
Elias Ibn Ziaten Cerezo <elias2603@hotmail.com>
2020-04-06 20:23:26 +0200

    Adding relu as activation function

--- neuroevolution/activation_functions.py
+++ neuroevolution/activation_functions.py
@@ -1,7 +1,16 @@
 import numpy as np
 
 def sigmoid(x):
-    return 1/(1+np.exp(-x))
+    return 1.0/(1.0+np.exp(-x))
 
 def sigmoid_der(x):
     return sigmoid(x)*(1-sigmoid(x))
+
+def relu(x):
+    return np.maximum(0,x)
+
+def relu_der(x):
+    result = 0
+    if x > 0:
+        result = 1
+    return result
