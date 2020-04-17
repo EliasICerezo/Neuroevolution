f984ba72a3c15d0fe65c0585a2096880d0c5ef57
Elias Ibn Ziaten Cerezo <elias2603@hotmail.com>
2020-04-17 21:06:27 +0200

    Implementing a random search optimized neural network.

---
+++ neuroevolution/networks/random_search_nn.py
@@ -0,0 +1,25 @@
+from neuroevolution.networks.basic_neural_network import BasicNeuralNetwork
+from neuroevolution.error_functions import crossentropy_loss
+import numpy as np
+class RandomSearchNeuralNetwork(BasicNeuralNetwork):
+  
+  def train(self, inputs: np.ndarray, targets: np.ndarray, epochs: int):
+    best_individual = self.params
+    activated_results = self.calculate_feed_forward(inputs, best_individual)
+    best_individual['loss'] = crossentropy_loss(targets,activated_results)
+    self.loss.append(best_individual['loss'])
+    for i in range(epochs):
+      new_individual = {}
+      self.initialize_weithts_and_biases(new_individual)
+      activated_results = self.calculate_feed_forward(inputs, new_individual)
+      new_individual['loss'] = crossentropy_loss(targets,activated_results)
+      if new_individual['loss'] < best_individual['loss']:
+        best_individual = new_individual
+        self.loss.append(new_individual['loss'])
+
+
+  def generate_random_individual(self):
+    individual = {}
+    self.initialize_weithts_and_biases(individual)
+    return individual
+    
