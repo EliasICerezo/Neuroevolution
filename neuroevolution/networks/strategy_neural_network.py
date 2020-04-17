a7a1fd9f09a1849383f32ae7446f19baaa596970
Elias Ibn Ziaten Cerezo <elias2603@hotmail.com>
2020-04-17 13:24:15 +0200

    Implementation of the strategy neural network

--- neuroevolution/networks/strategy_neural_network.py
+++ neuroevolution/networks/strategy_neural_network.py
@@ -1,17 +1,20 @@
 from neuroevolution.networks.genetic_neural_network import GeneticNeuralNetwork
 from neuroevolution.activation_functions import sigmoid
-import numpy
+import numpy as np
 
 
 class StrategyNeuralNetwork(GeneticNeuralNetwork):
   def __init__(self, layers:list, num_of_classes:int, input_size:int,
                activation_functs = None, pop_size = 50, sigma = 0.1, lr = 0.001):
     self.learning_rate = lr
-    self.pop_size = pop_size
     self.sigma = sigma
+    self.pop_size = pop_size
+    self.max_pop_size = pop_size
     self.layers = layers
-
     self.population = {}
+    self.additions = {}
+    if self.learning_rate <= 0:
+      raise AttributeError("The learning rate parameter can't be 0 or lower")
     if layers[-1] != num_of_classes:
       raise AttributeError("The number of classes should match the last layer")
     if layers[0] != input_size:
@@ -24,11 +27,29 @@ class StrategyNeuralNetwork(GeneticNeuralNetwork):
     self.initialize_weithts_and_biases()
   
   def train(self, inputs: np.ndarray, targets: np.ndarray, epochs: int):
-    # TODO: Generate samples in a uniform way
-    # TODO: Calculate a jittered version of it
-    # TODO: Standrize the rewards
-    # TODO: Apply them using the learning rate ponderation
-    
+    for i in range(epochs):
+      self.mutate_population(sigma=self.sigma)
+      # Normalize the mutations
+      self.normalize_mutations()
+      # Make the selection
+      self.population.update(self.additions)
+      activated_results = self.evolved_feed_forward(inputs)
+      self.calculate_loss(activated_results,targets)
+      self.sort_population()
+      self.selection_operator(1)
 
+  def normalize_mutations(self):
+    for (k,v) in self.additions.items():
+      for i in range(len(self.layers)-1):
+        if (v['W{}'.format(i+1)]).shape != (1,1):
+          v['W{}'.format(i+1)] = (v['W{}'.format(i+1)] - np.mean(v[
+              'W{}'.format(i+1)])) / np.std(v['W{}'.format(i+1)])
+        if (v['b{}'.format(i+1)]).shape != (1,1) and (
+            v['b{}'.format(i+1)]).shape != (1,):
+          v['b{}'.format(i+1)] = (v['b{}'.format(i+1)] - np.mean(v[
+              'b{}'.format(i+1)])) / np.std(v['b{}'.format(i+1)])
+        
+  
+  
   
 
