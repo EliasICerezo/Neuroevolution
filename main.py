3a3bd0c06821ea9e65ff869088898d0f3266f345
MagicElyas <elias2603@hotmail.com>
2020-05-17 03:49:03 +0200

    Creating the basic strategy network. - At the moment it gets trapped into local minima.

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from neuroevolution.networks.basic_neural_network import BasicNeuralNetwork
 from neuroevolution.networks.genetic_neural_network import GeneticNeuralNetwork
 from neuroevolution.networks.annealed_neural_network import AnnealedNeuralNetwork
+from neuroevolution.networks.strategy_neural_network import StrategyNeuralNetwork
 import numpy as np
 
 if __name__ == "__main__":
     feature_set = np.array([[0,1],[0,0],[1,0],[1,1]])
-    labels = np.array([[0,0,0,1]])
+    labels = np.array([[1,0,1,0]])
     labels = labels.reshape(4,1)
     # print(output_layer)
     #Creating the actual nnet
-    nnet = GeneticNeuralNetwork(layers=[2,1], input_size=2, num_of_classes= 1)
+    nnet = StrategyNeuralNetwork(layers=[2,1], input_size=2, num_of_classes= 1)
     # print(nnet.params)
     # print(nnet.population.keys())
     # print(nnet.feed_forward)
-    nnet.train(feature_set,labels, 7000)
+    nnet.train(feature_set,labels, 50)
     vs = nnet.population.values()
     vs = list(vs)
     for i in vs: print(i['loss'])
--- neuroevolution/networks/strategy_neural_network.py
+++ neuroevolution/networks/strategy_neural_network.py
@@ -33,15 +33,14 @@ class StrategyNeuralNetwork(GeneticNeuralNetwork):
     self.learning_rate = lr
     self.sigma = sigma
     self.pop_size = pop_size
-    self.max_pop_size = pop_size
     self.layers = layers
     self.population = {}
     self.additions = {}
     if len(self.layers) == 0:
       raise AttributeError("Can't create a neural net without a single layer ")
-    if pop_size == 0:
+    if pop_size <1:
       raise AttributeError(
-        "Can't create a genetic neural net without a single individual")
+        "Can't create a genetic neural net with less than 1 individual")
     if self.learning_rate <= 0:
       raise AttributeError("The learning rate parameter can't be 0 or lower")
     if layers[-1] != num_of_classes:
@@ -51,7 +50,7 @@ class StrategyNeuralNetwork(GeneticNeuralNetwork):
     self.activation_functs = activation_functs
     if activation_functs is None:
       self.activation_functs = [sigmoid for i in range(len(self.layers))]
-    self.initialize_weithts_and_biases()
+    self.initialize_weithts_and_biases(pop_size=1)
   
   def train(self, inputs: np.ndarray, targets: np.ndarray, epochs: int):
     """Training loop, it mutates the weights, biases, selects and orders the
@@ -63,32 +62,47 @@ class StrategyNeuralNetwork(GeneticNeuralNetwork):
         targets {np.ndarray} -- Target array, or labels
         epochs {int} -- Number of iterations
     """
+    if len(self.population.keys()) != 1:
+      raise AttributeError("Can't have more than one solution in this state of the program")
+    activated_results = self.evolved_feed_forward(inputs)
+    self.calculate_loss(activated_results,targets)
+    original_solution = self.population[list(self.population.keys())[0]]
+    print("Initial loss: {}".format(original_solution['loss']))
     for i in range(epochs):
-      self.mutate_population(sigma=self.sigma)
-      # Normalize the mutations
-      self.normalize_mutations()
-      # Make the selection
-      self.population.update(self.additions)
-      self.additions = {}
+      self.initialize_weithts_and_biases(pop_size=self.pop_size)
+      self.jitter_population(original_solution)
       activated_results = self.evolved_feed_forward(inputs)
       self.calculate_loss(activated_results,targets)
-      self.sort_population()
-      self.selection_operator(1)
+      standard_losses = self.standarize_loss()
+      self.update_weights(standard_losses,original_solution)
+      self.additions = {}
+    
+    self.population = {"final_solution": original_solution}
+    activated_results = self.evolved_feed_forward(inputs)
+    self.calculate_loss(activated_results,targets)
 
-  def normalize_mutations(self):
-    """Function that normalizes the mutations done to the current population
-    """
-    for (k,v) in self.additions.items():
-      for i in range(len(self.layers)-1):
-        if (v['W{}'.format(i+1)]).shape != (1,1):
-          v['W{}'.format(i+1)] = (v['W{}'.format(i+1)] - np.mean(v[
-              'W{}'.format(i+1)])) / np.std(v['W{}'.format(i+1)])
-        if (v['b{}'.format(i+1)]).shape != (1,1) and (
-            v['b{}'.format(i+1)]).shape != (1,):
-          v['b{}'.format(i+1)] = (v['b{}'.format(i+1)] - np.mean(v[
-              'b{}'.format(i+1)])) / np.std(v['b{}'.format(i+1)])
-        
-  
-  
-  
+  def update_weights(self, standard_losses:np.array, original_solution:dict):
+    for i,v in enumerate(self.population.values()):
+      for (a,b) in v.items():
+        original_solution[a] = original_solution[a] + self.learning_rate/(
+            self.pop_size*self.sigma) * np.dot(b,standard_losses[i])
 
+  def standarize_loss(self):
+    vs = list(self.population.values())
+    losses = []
+    for i in vs:
+      losses.append(i['loss'])
+    losses = np.asarray(losses)
+    return (losses - np.mean(losses)) / np.std(losses)
+    
+
+  def jitter_population(self, original_solution:dict):
+    # breakpoint()
+    for (_,v) in self.population.items():
+      jittered_individual = {}
+      for (a,b) in v.items():
+        # breakpoint()
+        jittered_individual[a] = original_solution[a] + (self.sigma * b)
+      # breakpoint()
+      self.new_individual(jittered_individual)
+    self.population = self.additions
