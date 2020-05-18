f66f97605115d9efad81816ebf6b84efb54b6564
MagicElyas <elias2603@hotmail.com>
2020-05-18 10:41:15 +0200

    Renaming files

--- dataset_manipulation/helpers.py
+++ dataset_manipulation/helpers.py
@@ -51,7 +51,6 @@ def extract_inputs_and_labels(df:pd.DataFrame,
   Returns:
       tuple -- A tuple containing inputs and labels in that order
   """
-  # TODO: Add in a comment that this operation does not support multillabelling
   df_copy = df.copy()
   df_copy = df_copy.drop(columns=drop_list)
   labels = df_copy[labels_name].to_numpy()
